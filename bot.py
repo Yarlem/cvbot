cd695495ff62941e1927ca6194b8006d153cfd5c
Yarlem <yarlem1322@gmail.com>
2022-01-24 12:12:01 +0300

    выгрузка кода в открытый доступ

---
+++ bot.py
@@ -0,0 +1,98 @@
+#Блок загрузок сторонних модулей
+from config import TOKEN
+from config import api_key
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ParseMode
+from aiogram.utils import executor
+from googleapiclient.discovery import build
+import os
+import googleapiclient.errors
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=TOKEN)
+storage=MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+#Установка состояний
+class Form(StatesGroup):
+    name = State()
+#Возможность отмены
+def saving(list):
+    for i in list['items']:
+        return(i['id']['videoId'])
+
+YT = build('youtube', 'v3', developerKey=api_key)
+  
+
+#Команда для поиска
+@dp.message_handler(commands=['find'], state=None)
+async def find_command(message: types.Message):
+    await Form.name.set()
+    await message.reply('Что нужно найти?')
+
+#Получение данных от пользователя
+@dp.message_handler(state=Form.name)
+async def get_object(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['name'] = message.text
+        #Обработка запроса через API Youtube
+    
+    request = YT.search().list(part="snippet", maxResults=1, q=data['name'])
+    response = request.execute()   
+    ytlink = 'https://www.youtube.com/watch?v='+saving(response)
+    await message.answer(ytlink)
+    await state.finish()
+
+#Обработка запроса через API Youtube
+#YT = build('youtube', 'v3', developerKey=api_key)
+#request = YT.search().list(part="snippet", maxResults=1, q=name)
+#response = request.execute()
+
+#def saving(list):
+    #for i in list['items']:
+        #return(i['id']['videoId'])
+
+#сохраняем итоговый результат в переменной
+#ytlink = 'https://www.youtube.com/watch?v='+saving(response) 
+
+
+
+
+
+#Установка команд для бота
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer('Привет! \nЭто бот-приложение к резюме. \nВведите /help для получения списка команд')
+
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await message.answer('/find - ищет на Youtube ролик по названию. \nНапример: /find portugal the man feel it still')
+    await message.answer('/info - для подробного описания целей этого бота')
+    await message.answer('/git - для получения доступа к коду бота')
+
+@dp.message_handler(commands=['info'])
+async def info_command(message: types.Message):
+    await message.answer('Бот создан, чтобы продемонстрировать навыки работы с API на примере работы с Telegram и Youtube. А так же понимание работы с Git, открытый доступ к коду позволяет оценить качество выполнения этого маленького проекта')
+
+@dp.message_handler(commands=['git'])
+async def git_command(message: types.Message):
+    await message.answer(' ВСТАВИТЬ ССЫЛКУ ')
+
+
+
+
+
+#добавь блок кода для каждой команды
+
+
+#Выкинуть на гит
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
